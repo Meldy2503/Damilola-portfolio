1d853ed5943ada65d234eddec20fd93a1e493209
meldy2503 <emelder.charles25@gmail.com>
2024-03-05 00:06:08 +0100

    feat: added summary component

--- src/app/components/home/summary.tsx
+++ src/app/components/home/summary.tsx
@@ -1,9 +1,72 @@
-import React from 'react'
+"use client";
+
+import { expertiseData } from "@/app/utils/constants";
+import Title from "@/app/utils/heading";
+import Tags from "@/app/utils/tags";
+import Wrapper from "@/app/utils/wrapper";
+import { Box, Flex, Heading, Icon, Text } from "@chakra-ui/react";
+import Image from "next/image";
+import authurImg from "../../assets/images/summary.webp";
+import { BsFillArrowRightCircleFill } from "react-icons/bs";
+import { Button } from "@/app/utils/button";
 
 const Summary = () => {
   return (
-    <div>Summary</div>
-  )
-}
+    <Wrapper bg="brand.150">
+      <Flex       direction={{base:'column', md:'row'}}
+>
+        <Flex
+          w={{ base: "100%", md: "50%" }}
+          className="linear-bg"
+          justify={'center'}
+          direction='column'
+          px={{ base: "2rem", xl: '5rem' }}
+          py='2rem'
+          borderRadius={{base:"3rem 3rem 0 0", md: "3rem 0 0 3rem"}} 
+        >
+          <Text py="2rem">Just before you go....</Text>
 
-export default Summary
+          <Heading  fontSize={{ base: "2rem", lg: "2.6rem"}} fontWeight={"600"} lineHeight={1.4}>
+            Certainly, with all you’ve seen so far i affirm that I've
+            effectively conveyed that I possess the appropriate skill set to
+            significantly boost your revenue{" "}
+          </Heading>
+
+          <Text py="2rem">In that case;</Text>
+          <Button path="/">
+            <Flex gap="7rem" align={"center"}>
+              <Text>Hire me</Text>
+
+              <Icon
+                as={BsFillArrowRightCircleFill}
+                w={12}
+                h={12}
+                color="brand.250"
+              />
+            </Flex>
+          </Button>
+        </Flex>
+        <Box
+          w={{ base: "100%", md: "50%" }}
+          borderRadius={{base:"0 0 3rem 3rem", md: "0 3rem 3rem 0"}} 
+
+        >
+          <Image
+            src={authurImg}
+            alt="author's photo"
+            width={800}
+            height={800}
+            style={{
+              maxWidth: "100%",
+              objectFit: "cover",
+              objectPosition: "center",
+               height: '100%'
+            }}
+          />
+        </Box>
+      </Flex>
+    </Wrapper>
+  );
+};
+
+export default Summary;
